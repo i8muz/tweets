c5f834fc49fbc8eaac3d1134613ca8280c4fc50e
i8muz <chaoticph@gmail.com>
2019-03-02 03:22:14 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1956,
-	"created_at": "Sun Feb 24 18:22:13 +0000 2019",
+	"tweets": 1957,
+	"created_at": "Fri Mar 01 18:22:14 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_03.js
@@ -0,0 +1,177 @@
+Grailbird.data.tweets_2019_03 = 
+[
+	{
+		"created_at": "Thu Feb 28 17:45:27 +0000 2019",
+		"id": 1101176568754421800,
+		"id_str": "1101176568754421765",
+		"full_text": "The music world has lost a legend. RIP, André Previn. https://t.co/v79pUs8Uyp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			53
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1101176525695664100,
+					"id_str": "1101176525695664128",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/D0gqN0EWwAAD_Ok.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0gqN0EWwAAD_Ok.jpg",
+					"url": "https://t.co/v79pUs8Uyp",
+					"display_url": "pic.twitter.com/v79pUs8Uyp",
+					"expanded_url": "https://twitter.com/ClassicFM/status/1101176568754421765/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 823,
+							"h": 540,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 446,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 823,
+							"h": 540,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1101176525695664100,
+					"id_str": "1101176525695664128",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/D0gqN0EWwAAD_Ok.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0gqN0EWwAAD_Ok.jpg",
+					"url": "https://t.co/v79pUs8Uyp",
+					"display_url": "pic.twitter.com/v79pUs8Uyp",
+					"expanded_url": "https://twitter.com/ClassicFM/status/1101176568754421765/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 823,
+							"h": 540,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 446,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 823,
+							"h": 540,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 64714144,
+			"id_str": "64714144",
+			"name": "Classic FM",
+			"screen_name": "ClassicFM",
+			"location": "UK",
+			"description": "The official Twitter home of Classic FM, the UK's favourite classical music station",
+			"url": "http://t.co/K8ky6A98dv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/K8ky6A98dv",
+							"expanded_url": "http://www.classicfm.com",
+							"display_url": "classicfm.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 134715,
+			"friends_count": 19163,
+			"listed_count": 1633,
+			"created_at": "Tue Aug 11 13:34:00 +0000 2009",
+			"favourites_count": 8576,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 90691,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1062304566144851970/0EoSFKFW_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1062304566144851970/0EoSFKFW_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/64714144/1542887302",
+			"profile_link_color": "EE000C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 445,
+		"favorite_count": 958,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The music world has lost a legend. RIP, André Previn. https://t.co/v79pUs8Uyp"
+	}
+]
